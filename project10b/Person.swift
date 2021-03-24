f3f3ba0da201b2dcc844c22e5c43ae45e4e19f41
Joey <yuuttoo@gmail.com>
2021-03-24 17:53:42 +0800

    Initial Commit

---
+++ project10b/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  project10b
+//
+//  Created by mac on 2021/3/24.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String){
+        self.name = name
+        self.image = image
+    }
+}
---
+++ project10b/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  project10b
+//
+//  Created by mac on 2021/3/24.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
